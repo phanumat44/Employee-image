45acf701c2988c6d2042c9deb0dee9d9e446b2a8
phanumat44 <96937316+phanumat44@users.noreply.github.com>
2022-09-26 02:55:10 +0700

    update button

--- components/addUserForm.js
+++ components/addUserForm.js
@@ -137,11 +137,27 @@ export default function AddUserForm(){
              Add <span className="px-1"><BiPlus size={24}></BiPlus></span>
             </button>
            
-            <p>
+            {/* <p>
             <input onChange={handleOnChange} type="file" name="file" />
           </p>
-          
-          <img src={imageSrc} />
+           */}
+
+          <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-full shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-gray-50" >
+        <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+        </svg>
+        <span class="mt-2 text-base leading-normal">Select a file</span>
+        <input type='file'  onChange={handleOnChange} class="hidden" name="file" />
+    </label>
+
+    
+
+
+
+          <img src={imageSrc ? imageSrc : "https://cdn-icons-png.flaticon.com/512/3177/3177440.png"}
+          className="max-w-64 max-h-64 w-64 rounded-full"
+          />
+
           
           {imageSrc && !uploadData && (
             <p>
